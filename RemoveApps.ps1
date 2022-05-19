48a59f04e8b1d469eff0fd5ead785984b0e911e3
Maxim Libant <mlibant+dev@gmail.com>
2022-05-19 12:03:46 -0400

    Update RemoveApps.ps1

--- RemoveApps.ps1
+++ RemoveApps.ps1
@@ -12,9 +12,28 @@ Start-Transcript C:\logs\$appName -Force
 #Region RemoveHP-Software
 $appList = Get-AppxPackage -allusers | where-object {$_.name -match "HP"}
 
-foreach ($app in $appList) {
-    Remove-AppxPackage -package $app -erroraction silentlycontinue
-    Write-Output('removing ' + $app + '...')
+foreach ($app in $appList) {    
+    try {
+    
+        Remove-AppxPackage -package $app -erroraction silentlycontinue
+        Write-Output('removing ' + $app + '...')
+    }
+    catch {
+        <#Do this if a terminating exception happens#>
+        $fail = $_.exception.message   
+    }
+    finally {
+        <#Do this after the try block regardless of whether an exception occurred or not#>
+        if ($fail) {
+
+            Write-Host $fail
+            Stop-Transcript
+            throw $fail
+
+        } else {
+            Write-Host "Apps removed, moving on."
+        }
+    }
 }
 #endRegion
 
@@ -24,7 +43,7 @@ $tcoPath = 'C:\Users\Public\Desktop\TCO Certified.lnk'
 if (Test-Path -path $tcoPath){
 
     remove-item -path $tcoPath
-
+    Write-Output('removing ' + $tcoPath + '...')
 }
 
 #remove shortcuts
@@ -32,19 +51,23 @@ $shortcut = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\HP Sure Click 
 if (Test-Path $shortcut) {
 
     Remove-Item $shortcut
+    Write-Output('removing ' + $shortcut + '...')
 }
 
 $shortcut = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\HP Wolf Security.lnk'
 if (Test-Path $shortcut) {
 
     Remove-Item $shortcut
+    Write-Output('removing ' + $shortcut + '...')
 }
 
 $shortcut = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\HP Documentation.lnk'
 if (Test-Path $shortcut) {
 
     Remove-Item $shortcut
+    Write-Output('removing ' + $shortcut + '...')
 }
 #endRegion
 
+Write-Output('Script completed succsefully')
 Stop-Transcript
